45811282121f74cb22c1d82547de1f964a7ca500
Tony Blank <me@tonyblank.com>
2015-01-01 15:51:20 -0700

    added test file for the flow meter...

---
+++ flow_meter_test/test_flow_meter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+
+import flowmeter
+
+
+GPIO.setmode(GPIO.BCM) # use real GPIO numbering
+GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+fm = flowmeter.FlowMeter('metric', 'beer')
+fm.enabled = True
+
+
+def doAClick(channel):
+  currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+  if fm.enabled == True:
+    fm.update(currentTime)
+
+
+GPIO.add_event_detect(23, GPIO.RISING, callback=doAClick, bouncetime=20)
+
+
+while True:
+  currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+
+  if (fm.thisPour > 0.23 and currentTime - fm.lastClick > 5000): # 5 seconds of inactivity causes a print msg
+    msg = "Someone just blew %s of %s with a total breath of %s.\n" % (fm.getFormattedThisPour(), fm.getBeverage(), fm.getFormattedTotalPour())
+    fm.thisPour = 0.0
+    print msg
+
